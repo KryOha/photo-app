abddf0ece2816e7d86d503761ee00d62c8f59c72
learningrugy1 <lasvegas.1997@yahoo.com>
2016-02-18 22:10:15 -0800

    added region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,11 @@ if Rails.env.production?
        :provider => 'AWS',
        :aws_access_key_id => ENV['S3_ACCESS_KEY'],
        :aws_secret_access_key => ENV['S3_SECRET_KEY']
+       host: 'S#.amazonaws.com',
+       region: 'us-west-2'
      }
      config.fog_directory = ENV['S3_BUCKET']
 
    end
-    
+
 end
